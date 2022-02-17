ecd336e5f605e3622d99e828c675aba3736e9230
Layne <Layne@atguigu.com>
2022-02-17 13:21:06 +0800

    hot-fix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix1");
+        System.out.println("hello hot-fix2");
     }
 }
